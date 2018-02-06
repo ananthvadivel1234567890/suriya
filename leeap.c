cd10c2663dabafdaaaae7f0feb3d36fd8b82de5d
ananthvadivel1234567890 <35652942+ananthvadivel1234567890@users.noreply.github.com>
2018-02-06 16:30:19 +0530

    Create leeap.c

---
+++ leeap.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int main();
+    {
+        int x;
+        printf("enters the numbers");
+        scanf("%d",&X);
+        if(X%4==0);
+        {
+            printf("leap year");
+        }
+        else
+        {
+            printf("not leap year");
+        }
+    }
